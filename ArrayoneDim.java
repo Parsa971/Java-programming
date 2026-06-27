607521ecbaa16650a527cac164f30aba084948c3
Ali <aliparsajaghori@gmail.com>
2026-06-27 08:59:26 +0430

    one dimentional array:

---
+++ ArrayoneDim.java
@@ -0,0 +1,104 @@
+package com.mycompany.mavenproject1.arrayone.dim;
+import java.util.Scanner;
+public class ArrayoneDim {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+//  Question 1:    
+//        System.out.println("Enter a number: ");
+//        double x = input.nextDouble();
+//        System.out.println("Enter five elements of array: ");
+//        double[] a = new double[5];
+//        for(int i=0; i<a.length;i++){
+//            a[i] = input.nextDouble();
+//        }
+//        int c=0;
+//        for(int i=0; i<a.length;i++){
+//            if(a[i]==x) c++;
+//        }
+//        System.out.println("number of same nums: " +c);
+     
+//   Question 2: 
+
+//        double[] a = new double[4];
+//        double[] b = new double[3];
+//        double[] c = new double[a.length+ b.length];
+//        System.out.println("Enter the first array");
+//        for(int i=0; i<a.length; i++){
+//            a[i]= input.nextDouble();
+//        }
+//        System.out.println("Enter the second array");
+//        for(int i=0; i<b.length; i++){
+//            b[i]= input.nextDouble();
+//        }
+//       
+//        for(int i=0; i<a.length; i++){
+//            c[i] = a[i];
+//        }
+//        for(int i=0; i<b.length; i++){
+//            c[a.length+i] = b[i];
+//        }
+//        System.out.println("-----------------");
+//        for(int i=0; i<c.length; i++){
+//            System.out.println(c[i]);
+//        }
+   // Question 3:
+   
+//        int[] a = new int[4];
+//        int[] b = new int[3];
+//        int[] c = new int[a.length];
+//        System.out.println("Enter the first array");
+//        for(int i=0; i<a.length; i++){
+//            a[i]= input.nextInt();
+//        }
+//        System.out.println("Enter the second array");
+//        for(int i=0; i<b.length; i++){
+//            b[i]= input.nextInt();
+//        }
+//        int k=0;
+//        for(int i=0;i<a.length;i++){
+//            boolean flag = true;
+//            for(int j=0; j<b.length;j++){
+//                if(a[i]==b[j]) flag = false; 
+//            }
+//            if(flag){
+//                c[k]= a[i];
+//                k++;
+//            }
+//        }
+//        for(int i=0;i<c.length;i++)
+//            System.out.println(c[i]);
+
+//     Question 4: 
+
+//        int[] a = new int[4];
+//        int[] b = new int[4];
+//        
+//        System.out.println("Enter the first array");
+//        for(int i=0; i<a.length; i++){
+//            a[i]= input.nextInt();
+//        }
+//        System.out.println("Enter the second array");
+//        for(int i=0; i<b.length; i++){
+//            b[i]= input.nextInt();
+//        }
+//        boolean flag2 = true;
+//        int k=0;
+//        for(int i=0; i<a.length;i++){
+//            boolean flag = false;
+//            for(int j=0;j<b.length;j++){
+//                if(a[i]==b[j])  flag = true;
+//                
+//            }
+//            if(!flag){
+//                flag2=false;
+//                break;
+//            }
+//             
+//        }
+//        if(flag2==true) System.out.println("true");
+//        else    System.out.println("false");
+        
+   
+    }
+}
